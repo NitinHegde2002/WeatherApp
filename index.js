60f5f9e3e9a1ed04869413638048ebf6f8516fd6
Nitin Hegde <92938023+NitinHegde2002@users.noreply.github.com>
2024-02-27 16:14:29 +0530

    Cleanup index.js

--- index.js
+++ index.js
@@ -30,32 +30,25 @@ search.addEventListener("click", () => {
       const image = document.querySelector(".weather-box img");
       const temperature = document.querySelector(".weather-box .temperature");
       const description = document.querySelector(".weather-box .description");
-      const humidity = document.querySelector(
-        ".weather-details .humidity span"
-      );
+      const humidity = document.querySelector(".weather-details .humidity span");
       const wind = document.querySelector(".weather-details .wind span");
 
       switch (json.weather[0].main) {
         case "Clear":
           image.src = "images/clear.png";
           break;
-
         case "Rain":
           image.src = "images/rain.png";
           break;
-
         case "Snow":
           image.src = "images/snow.png";
           break;
-
         case "Clouds":
           image.src = "images/cloud.png";
           break;
-
         case "Haze":
           image.src = "images/mist.png";
           break;
-
         default:
           image.src = "";
       }
